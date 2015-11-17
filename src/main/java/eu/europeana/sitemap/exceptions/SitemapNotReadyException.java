a421fdc71f56888126853bfe3d5cb7cd54c430a7
ymamakis <yorgos.mamakis@europeana.eu>
2015-11-17 17:40:39 +0100

    Initial commit

---
+++ src/main/java/eu/europeana/sitemap/exceptions/SitemapNotReadyException.java
@@ -0,0 +1,7 @@
+package eu.europeana.sitemap.exceptions;
+
+/**
+ * Created by ymamakis on 11/16/15.
+ */
+public class SitemapNotReadyException extends RuntimeException {
+}
---
+++ src/main/java/eu/europeana/sitemap/web/SitemapGenerationController.java
@@ -0,0 +1,29 @@
+package eu.europeana.sitemap.web;
+
+
+import eu.europeana.sitemap.exceptions.SitemapNotReadyException;
+import eu.europeana.sitemap.service.SitemapService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+
+/**
+ * Created by ymamakis on 11/16/15.
+ */
+@RestController
+@RequestMapping("/")
+public class SitemapGenerationController {
+
+    @Resource
+    private SitemapService service;
+
+    @RequestMapping(value = "generate", method = RequestMethod.GET)
+    public @ResponseBody String generate() throws SitemapNotReadyException{
+        service.generate();
+        return "OK";
+    }
+}
