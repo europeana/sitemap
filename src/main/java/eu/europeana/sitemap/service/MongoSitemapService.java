0948b51c3cb65a68f8ae225da02eb24cc6a61d32
ymamakis <yorgos.mamakis@europeana.eu>
2016-07-21 15:14:18 +0200

    feat/2135- Fixed lastmod

--- src/main/java/eu/europeana/sitemap/service/MongoSitemapService.java
+++ src/main/java/eu/europeana/sitemap/service/MongoSitemapService.java
@@ -6,9 +6,10 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import eu.europeana.sitemap.exceptions.SitemapNotReadyException;
-import eu.europeana.sitemap.swift.SwiftProvider;
 import eu.europeana.sitemap.mongo.MongoProvider;
+import eu.europeana.sitemap.swift.SwiftProvider;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.jclouds.io.Payload;
 import org.jclouds.io.payloads.StringPayload;
 import org.jclouds.openstack.swift.v1.domain.ObjectList;
@@ -51,11 +52,14 @@ public class MongoSitemapService implements SitemapService {
     private static final String URLSET_HEADER_CLOSING = "</urlset>";
     private static final String PRIORITY_OPENING = "<priority>";
     private static final String PRIORITY_CLOSING = "</priority>";
+    private static final String LASTMOD_OPENING = "<lastmod>";
+    private static final String LASTMOD_CLOSING = "</lastmod>";
     private static String status = "initial";
     private static final String MASTER_KEY = "europeana-sitemap-index-hashed.xml";
     private static final int WEEKINSECONDS = 1000 * 60 * 60 * 24 * 7;
     private static final Logger log= Logger.getLogger(MongoSitemapService.class.getName());
     public void generate() throws SitemapNotReadyException {
+        log.info("Status :" + status);
         if (!(status.equalsIgnoreCase("working"))) {
             if (!checkExists()) {
                 status = "working";
@@ -66,6 +70,8 @@ public class MongoSitemapService implements SitemapService {
                 fields.put("about", 1);
                 fields.put("europeanaCompleteness", 1);
                 DBCursor cur = col.find(query, fields).batchSize(45000);
+                log.info ("Got cursor");
+                log.info("Cursor hasNext:" +cur.hasNext());
                 int i = 0;
                 StringBuilder master = new StringBuilder();
                 master.append(XML_HEADER).append(LN);
@@ -77,11 +83,14 @@ public class MongoSitemapService implements SitemapService {
 
                     DBObject obj = cur.next();
                     String about = obj.get("about").toString();
+                    long date =obj.get("timestampUpdated")!=null? (long)obj.get("timestampUpdated"):0;
+                    String update = DateFormatUtils.format(date, DateFormatUtils.ISO_DATE_FORMAT.getPattern());
                     int completeness = Integer.parseInt(obj.get("europeanaCompleteness").toString());
                     slave.append(URL_OPENING).append(LN).append(LOC_OPENING).append(LN).append(PORTAL_URL)
                             .append(about).append(HTML).append(LN).append(LOC_CLOSING).append(PRIORITY_OPENING)
                             .append(completeness > 9 ? "1.0" : "0." + completeness)
-                            .append(PRIORITY_CLOSING).append(LN).append(URL_CLOSING).append(LN);
+                            .append(PRIORITY_CLOSING).append(LN).append(LASTMOD_OPENING).append(update)
+                            .append(LASTMOD_CLOSING).append(LN).append(URL_CLOSING).append(LN);
                     if (i>0 && (i % 45000 == 0 || !cur.hasNext())) {
                         String indexEntry = new StringBuilder().append(INDEX_ENTRY).append(i - 45000).append(TO).append(i).toString();
                         master.append(SITEMAP_OPENING).append(LN).append(LOC_OPENING).append(StringEscapeUtils.escapeXml("http://www.europeana.eu/portal/" + indexEntry))
@@ -106,8 +115,9 @@ public class MongoSitemapService implements SitemapService {
     }
 
     private boolean checkExists() {
-
-        return swiftProvider.getObjectApi().getWithoutBody(MASTER_KEY)!=null;
+        SwiftObject exists = swiftProvider.getObjectApi().getWithoutBody(MASTER_KEY);
+        log.info("Exists: "+ (exists!=null));
+        return exists!=null;
     }
 
     private StringBuilder initializeSlaveGeneration() {
@@ -137,6 +147,7 @@ public class MongoSitemapService implements SitemapService {
     }
 
     public void delete(){
+        swiftProvider.getObjectApi().delete(MASTER_KEY);
         ObjectList list = swiftProvider.getObjectApi().list();
         log.info("Files to remove: "+ list.size());
         int i=0;
