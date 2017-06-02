c85db6cb1a0360dcf91d2265c3c9c180762d67a7
Patrick Ehlert <patrick.ehlert@europeana.eu>
2017-06-02 19:55:18 +0200

    Add extra functionality to see result (see list and content of stored files)
    This commit also includes various fixes:
    - Fix set status to done when update process fails
    - Fixed potential memory leak when reloading application (s3 was not shutdown)
    - Fixed problem connecting to Mongo with empty username/password
    - Add sitemap.user.properties to prevent accidentally commiting username/password
    - Added documentation
    - Removed unused classes (redis/jedis)
    - Code cleanup / fix sonar warnings

--- src/main/java/eu/europeana/sitemap/exceptions/SitemapNotReadyException.java
+++ src/main/java/eu/europeana/sitemap/exceptions/SitemapNotReadyException.java
@@ -1,6 +1,7 @@
 package eu.europeana.sitemap.exceptions;
 
 /**
+ * Exception that indicates that there is already an update running
  * Created by ymamakis on 11/16/15.
  */
 public class SitemapNotReadyException extends RuntimeException {
