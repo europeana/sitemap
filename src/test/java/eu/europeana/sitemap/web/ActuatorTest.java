f0b5b4ed6ea98841f6ec13fc13b07dacc105bd2c
Patrick Ehlert <patrick.ehlert@europeana.eu>
2022-08-16 15:01:51 +0200

    Fix failing unit test

--- src/test/java/eu/europeana/sitemap/web/ActuatorTest.java
+++ src/test/java/eu/europeana/sitemap/web/ActuatorTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * JUnit test for testing if the /actuator/info endpoint is available
  */
 @SpringBootTest
+@TestPropertySource("classpath:sitemap-test.properties")
 @AutoConfigureMockMvc
 public class ActuatorTest {
 
